7327961a8eda9489dd8426e24306854ed3a6a825
leon <leon991214@gmail.com>
2024-09-03 12:57:43 +0800

    refactor: 细分文件夹

--- JavaScript/函数缓存.js
+++ JavaScript/函数缓存.js
@@ -1,16 +1,16 @@
-function sum(a,b) {
+function sum(a, b) {
   console.log('excute sum')
-  return a+b
+  return a + b
 }
 
 function memoize(func) {
   let cache = new Map()
-  const memoizeFn = function(...args) {
+  const memoizeFn = function (...args) {
     let key = JSON.stringify(args)
     if (!cache.has(key)) cache.set(key, func.apply(this, args))
     return cache.get(key)
   }
-  memoizeFn.deleteCache = function(...args){
+  memoizeFn.deleteCache = function (...args) {
     let key = JSON.stringify(args)
     if (cache.has(key)) cache.delete(key)
   }
@@ -20,15 +20,12 @@ function memoize(func) {
 
 const sumWapper = memoize(sum)
 const num1 = sumWapper(1, 2)
-console.log("num1 => ", num1);// num1会输出excute sum
-console.log('----------------------');
+console.log('num1 => ', num1) // num1会输出excute sum
+console.log('----------------------')
 const num2 = sumWapper(1, 2)
-console.log("num2 => ", num2); // 不会输出excute sum
-console.log('----------------------');
-sumWapper.deleteCache(1,2)
+console.log('num2 => ', num2) // 不会输出excute sum
+console.log('----------------------')
+sumWapper.deleteCache(1, 2)
 const num3 = sumWapper(1, 2)
-console.log("num2 => ", num3); // 不会输出excute sum
-console.log('----------------------');
-
-
-
+console.log('num2 => ', num3) // 不会输出excute sum
+console.log('----------------------')
--- JavaScript/手写某个方法/手写trim方法.ts
+++ JavaScript/手写某个方法/手写trim方法.ts
@@ -1,8 +1,8 @@
 // 实现一个方法，清除字符串前后的空格。
 let str = '   AAAA  BBBB '
 // console.log(str.trim());
-const myTrim = (str) => {
+const myTrim = (str: string) => {
   const reg = /^\s* | \s*&/g
   return str.replace(reg, '')
 }
-console.log(myTrim(str));
+console.log(myTrim(str))
